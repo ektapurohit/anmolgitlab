573d8b3c9eb59f156bf0571634ebf2d1d07793f6
ekta purohit <ektapurohit@live.com>
2024-04-10 20:19:45 +0200

    commiting terraform code

---
+++ providers.tf
@@ -0,0 +1,8 @@
+provider "azurerm" {
+  subscription_id            = "0cfe2870-d256-4119-b0a3-16293ac11bdc"
+  tenant_id                  = "84f1e4ea-8554-43e1-8709-f0b8589ea118"
+  client_id                  = "d5a4e8f9-4720-4c2d-b78e-8b89a65c1012"
+  client_secret              = "Frw8Q~4oBqAxKYonmhnMiC5J.oHzekQd0g66pc9s"
+  skip_provider_registration = true
+  features {}
+}
